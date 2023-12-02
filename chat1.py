3218f1a577b61874324c889b7ea5d844052e1d7f
AkshayGumme <150417034+AkshayGumme@users.noreply.github.com>
2023-12-02 16:20:18 +0530

    chat1.py

--- chat1.py
+++ chat1.py
@@ -1,68 +1,50 @@
 import dash
-from dash import html, dcc
+from dash import dcc, html
+from dash.dependencies import Input, Output
 
 app = dash.Dash(__name__)
 
-app.layout = html.Div(
-    children=[
-        html.Div(
-            id='sidebar',
-            style={
-                'width': '250px',
-                'backgroundColor': '#075e54',
-                'color': 'white',
-                'padding': '20px',
-            },
-            children=[
-                # Sidebar content goes here
-                html.H2('Sidebar'),
-            ]
-        ),
-        html.Div(
-            id='chat',
-            style={'flex': 1, 'padding': '20px', 'overflowY': 'scroll', 'flexDirection': 'column'},
-            children=[
-                # Chat messages go here
-                html.Div('Sender: Hello!'),
-                html.Div('Receiver: Hi there!'),
-                html.Div('Sender: How are you?'),
-            ]
-        ),
-        html.Div(
-            id='input-container',
-            style={
-                'display': 'flex',
-                'flex-direction':"column",
-                'justifyContent': 'space-between',
-                'padding': '20px',
-                'backgroundColor': 'white',
-                'boxShadow': '0px -5px 5px -5px #000000',
-            },
-            children=[
-                dcc.Input(
-                    id='input',
-                    type='text',
-                    placeholder='Type a message...',
-                    style={'flex': 1, 'padding': '10px', 'border': 'none', 'borderRadius': '5px'}
-                ),
-                html.Button(
-                    id='send-button',
-                    children='Send',
-                    n_clicks=0,
-                    style={
-                        'backgroundColor': '#128c7e',
-                        'color': 'white',
-                        'padding': '10px 20px',
-                        'border': 'none',
-                        'borderRadius': '5px',
-                        'cursor': 'pointer',
-                    }
-                ),
-            ]
-        ),
-    ],
-    style={'display': 'flex','flex-direction':"column",}
+# Layout of the form
+app.layout = html.Div([
+    html.H1("Simple Form Example"),
+    
+    # Text Input
+    dcc.Input(
+        id='input-username',
+        type='text',
+        placeholder='Enter your username',
+        value=''
+    ),
+    
+    # Dropdown
+    dcc.Dropdown(
+        id='dropdown-country',
+        options=[
+            {'label': 'United States', 'value': 'US'},
+            {'label': 'Canada', 'value': 'CA'},
+            {'label': 'United Kingdom', 'value': 'UK'}
+        ],
+        value='US',
+        placeholder='Select a country'
+    ),
+    
+    # Submit Button
+    html.Button('Submit', id='button-submit', n_clicks=0),
+    
+    # Output Div
+    html.Div(id='output-message')
+])
+
+# Callback to update the output message
+@app.callback(
+    Output('output-message', 'children'),
+    [Input('button-submit', 'n_clicks')],
+    [dash.dependencies.State('input-username', 'value'),
+     dash.dependencies.State('dropdown-country', 'value')]
 )
+def update_output(n_clicks, username, country):
+    if n_clicks > 0:
+        return f"Submitted! Username: {username}, Country: {country}"
 
 if __name__ == '__main__':
     app.run_server(debug=True)
